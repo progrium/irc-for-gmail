b2fc902102d68622b50432a5be4bf8b8a594f1f7
Jeff Lindsay <progrium@gmail.com>
2012-06-10 01:27:26 -0700

    make sure it remembers if you had it minimized

--- src/gmail.js
+++ src/gmail.js
@@ -1,14 +1,17 @@
+var initialDisplay = localStorage['irc-container-display'];
+var ircUrl = chrome.extension.getURL('src/irc.html');
 $('body').append($('\
   <div id="irc-window">\
     <div id="irc-window-titlebar">\
       <img src="images/cleardot.gif" alt="Minimize">\
       <span id="irc-window-title">IRC</span>\
     </div>\
-    <iframe id="irc-container" src="'+chrome.extension.getURL('src/irc.html')+'"></iframe>\
+    <iframe id="irc-container" style="display: '+initialDisplay+';" src="'+ircUrl+'"></iframe>\
   </div>'));
 
 $('#irc-window-titlebar').bind('click', function() {
   $("#irc-container").toggle();
+  localStorage['irc-container-display'] = $('#irc-container').css('display');
   $("#irc-window-titlebar").removeClass("irc-alert");
 });
 
